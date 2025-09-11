14c2cc1c1ae4445d86476f335b63b376a41a12a6
Teejay2223 <tijjanishuaibmatopkm@gmail.com>
2025-09-11 14:45:53 +0100

    added new feature: service page

--- schema.sql
+++ schema.sql
@@ -62,6 +62,12 @@ CREATE TABLE IF NOT EXISTS payments (
     status TEXT DEFAULT 'Pending',
     FOREIGN KEY (order_id) REFERENCES orders (id)
 );
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL UNIQUE,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL
+);
 
 -- Products Table
 CREATE TABLE IF NOT EXISTS products (
